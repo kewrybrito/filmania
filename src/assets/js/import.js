7635c9601909afc1e32fd65f73745dd24d998669
Kewry Brito <kewrybrito7@gmail.com>
2025-05-07 15:54:56 -0300

    ok

--- src/assets/js/import.js
+++ src/assets/js/import.js
@@ -1,22 +1,25 @@
 
 window.addEventListener('DOMContentLoaded', () => {
+  const path = window.location.pathname;
+  const basePath = path.includes('/pages/') ? '../components/cabecalho.html' : './components/cabecalho.html';
 
-    fetch('components/cabecalho.html')  
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Erro ao carregar o cabeçalho');
-        }
-        return response.text();
-      })
-      .then(data => {
-        document.getElementById('header').innerHTML = data;  
-      })
-      .catch(error => console.error('Erro:', error));
+  fetch(basePath)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erro ao carregar o cabeçalho');
+      }
+      return response.text();
+    })
+    .then(data => {
+      document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Erro:', error));
 });
+
 
 window.addEventListener('DOMContentLoaded', () => {
 
-  fetch('components/rodape.html')  
+  fetch('../components/rodape.html')  
     .then(response => {
       if (!response.ok) {
         throw new Error('Erro ao carregar o cabeçalho');
